2a9cdb33a49ea922caba5a93675b2eb0189af0d4
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-08-26 06:17:27 +0530

    file added

---
+++ index.js
@@ -0,0 +1,10 @@
+//console.log("hello");
+//console.log("i like pizza" );
+
+//window.alert("this is an alert!");
+//window.alert("i like pizza!");
+
+//comments.
+
+document.getElementById("myH1").textContent="HELLO";
+document.getElementById("myp").textContent="world !";
