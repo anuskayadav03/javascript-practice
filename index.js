2362ebabbce4bf901156f4ec355c13007fa6d92d
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-08-26 06:41:16 +0530

    file updated

--- index.js
+++ index.js
@@ -9,22 +9,32 @@
 //document.getElementById("myH1").textContent="HELLO";
 //document.getElementById("myp").textContent="world !";
 
-let x=100;
-console.log(x);
-console.log(typeof(x));
+// let x=100;
+// console.log(x);
+// console.log(typeof(x));
 
-let cgpa=8.20;
-console.log(cgpa);
-console.log(typeof(cgpa));
+// let cgpa=8.20;
+// console.log(cgpa);
+// console.log(typeof(cgpa));
 
-console.log(`you score ${cgpa} in your exam`);
-console.log(`i have ${x} rupees`);
+// console.log(`you score ${cgpa} in your exam`);
+// console.log(`i have ${x} rupees`);
 
-let firstName ="anushka";
-console.log(firstName);
-console.log(typeof(firstName));
-console.log(`my name is ${firstName}`);
+// let firstName ="anushka";
+// console.log(firstName);
+// console.log(typeof(firstName));
+// console.log(`my name is ${firstName}`);
 
-let online=true;
-console.log(typeof(online));
-console.log(`bro is online: ${online}`);
+// let online=true;
+// console.log(typeof(online));
+// console.log(`bro is online: ${online}`);
+
+let fullName=" anuska yadav";
+let age= 20;
+let student = false;
+let phone =1234567891;
+
+document.getElementById("p1").textContent=fullName;
+document.getElementById("p2").textContent=age;
+document.getElementById("p3").textContent=student;
+document.getElementById("p4").textContent=phone ;
