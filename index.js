e2f288e64263bf8ced96e70d04dd66983647571b
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-08-28 15:12:33 +0530

    file updated

--- index.js
+++ index.js
@@ -84,9 +84,9 @@
     // console.log(age);
     // console.log(typeof(age));
 
-    let x="0";
-    let y="0";
-    let z="0";
+    let x="";
+    let y="";
+    let z="";
 
     x= Number(x);
     y=String(y);
