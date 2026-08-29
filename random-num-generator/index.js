9f71ace9da3693bc87b6330ebdc1aa8c13acc420
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-08-29 21:57:33 +0530

    file updated

--- random-num-generator/index.js
+++ random-num-generator/index.js
@@ -1,5 +1,16 @@
-const max=50;
-const min=1;
+// const max=50;
+// const min=1;
 
-let randomNum = Math.floor(Math.random() * (max - min))+1;
-console.log(randomNum);
+// let randomNum = Math.floor(Math.random() * (max - min))+1;
+// console.log(randomNum);
+
+const myButton = document.getElementById("myButton");
+const label = document.getElementById("label");
+const max=6;
+const min=1;
+let randomNum;
+
+myButton.onclick =function(){
+    randomNum = Math.floor(Math.random() *max)+min;
+    label.textContent = `the num is : ${randomNum}`;
+}
