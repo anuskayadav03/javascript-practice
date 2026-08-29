52d057a1115cba7e1483dad808d228f3816686a1
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-08-29 21:12:22 +0530

    file updated

---
+++ math/index.js
@@ -0,0 +1,14 @@
+
+//Math is a collection of properties
+// console.log(Math.PI);
+// console.log(Math.E);
+
+let x= 3.12;
+let y = 2;
+let z;
+// z = Math.round(x);
+// z=Math.floor(x);
+// z=Math.ceil(x);
+// z=Math.pow(x,y);
+z=Math.sqrt(x);
+console.log(z);
