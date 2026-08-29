2338220eeb27fe4e6d737d69a9ce9e949bd40b3e
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-08-29 21:20:48 +0530

    file updated

--- math/index.js
+++ math/index.js
@@ -3,20 +3,20 @@
 // console.log(Math.PI);
 // console.log(Math.E);
 
-let x= 3.12;
-let y = 2;
-let z=10;
-// z = Math.round(x);
-// z=Math.floor(x);
-// z=Math.ceil(x);
-// z=Math.pow(x,y);
-// z=Math.sqrt(x);
-// z=Math.abs(x);
-// z=Math.sin(x);
-// z=Math.cos(x);
-// z=Math.tan(x);
-// z=Math.sign(x);
+// let x= 3.12;
+// let y = 2;
+// let z=10;
+// // z = Math.round(x);
+// // z=Math.floor(x);
+// // z=Math.ceil(x);
+// // z=Math.pow(x,y);
+// // z=Math.sqrt(x);
+// // z=Math.abs(x);
+// // z=Math.sin(x);
+// // z=Math.cos(x);
+// // z=Math.tan(x);
+// // z=Math.sign(x);
 
-let max = Math.max(x, y, z);
-let min = Math.min(x, y, z);
-console.log(min);
+// let max = Math.max(x, y, z);
+// let min = Math.min(x, y, z);
+// console.log(min);
