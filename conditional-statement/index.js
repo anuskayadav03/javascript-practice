5a1cab52f5e54201fc9118711bef6b8697151411
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-08-30 15:16:54 +0530

    file updated

---
+++ conditional-statement/index.js
@@ -0,0 +1,16 @@
+// let age=13;
+// if(age>=25){
+//     console.log("you are old enough to enter the site");
+// }
+// else{
+//     console.log("you must be 18+");
+// }
+
+// let isStudent=false;
+// if(isStudent){
+//     console.log("you are a student");
+// }
+// else{
+//     console.log("you are not a student");
+// }
+
