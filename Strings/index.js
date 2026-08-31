62d92d3892e580ebdf0e29ede6dc68bd114d8690
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-08-31 21:58:49 +0530

    file updated

--- Strings/index.js
+++ Strings/index.js
@@ -1,3 +1,10 @@
-let username = "anuska";
-let name = username.charAt(1);
-console.log(name);
+let username = "      anuska   ";
+// let name = username.charAt(1);
+// console.log(name);
+
+// console.log(username.indexOf("a"));
+// console.log(username.length);
+
+// username = username.trim();
+// console.log(username)
+
