9565006b503555e79fd4c83d8cf5752b9a0473c2
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-01 19:57:48 +0530

    file updated

--- string-slicing/index.js
+++ string-slicing/index.js
@@ -1,13 +1,19 @@
-let username = "anushka yadav";
+// let username = "anushka yadav";
 
-let firstName = username.slice(0,7);
-let lastname = username.slice(8,13);
+// let firstName = username.slice(0,7);
+// let lastname = username.slice(8,13);
 
-let firstchar = username.slice(0,1);
-let lastchar = username.slice(12,13);
-let lastchar2 = username.slice(-1);
+// let firstchar = username.slice(0,1);
+// let lastchar = username.slice(12,13);
+// let lastchar2 = username.slice(-1);
 
-console.log(firstName);
+// console.log(firstName);
+// console.log(lastname);
+// console.log(firstchar);
+// console.log(lastchar2);
+
+const fullname = "anuska yadav";
+let firstname = fullname.slice(0, fullname.indexOf(" "));
+let lastname = fullname.slice( fullname.indexOf(" ")+1);
+console.log(firstname);
 console.log(lastname);
-console.log(firstchar);
-console.log(lastchar2);
