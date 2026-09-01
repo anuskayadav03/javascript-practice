44bed604705cc3a3102cb8e95fcef8e6d0e566b6
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-01 20:52:58 +0530

    file updated

---
+++ logical operators/index.js
@@ -0,0 +1,19 @@
+                   // ---LOGICAL OPERATORS ---
+
+const temp = 0;
+
+//AND OPERATOR
+if(temp>0 && temp ==0){
+    console.log("weather is good");
+}
+//OR OPERATOR
+else if(temp<=30 || temp==40){
+    console.log("weather is average");
+}
+//NOT OPERATOR
+else if(temp !=50){
+    console.log("weather is still good");
+}
+else{
+    console.log("weather is bad");
+}
