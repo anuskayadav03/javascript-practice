c7e2dd8b738882fa0b347d6b1865c7ce0be7f689
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-01 19:48:13 +0530

    file updated

--- string-slicing/index.js
+++ string-slicing/index.js
@@ -2,5 +2,11 @@ let username = "anushka yadav";
 
 let firstName = username.slice(0,7);
 let lastname = username.slice(8,13);
+
+let firstchar = username.slice(0,1);
+let lastchar = username.slice(12,13);
+
 console.log(firstName);
 console.log(lastname);
+console.log(firstchar);
+console.log(lastchar);
