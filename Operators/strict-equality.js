7fa37f241fc4ff607cb1d0d33eeb8cfe5d61d25b
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-02 21:46:38 +0530

    file updated

--- Operators/strict-equality.js
+++ Operators/strict-equality.js
@@ -1,7 +1,10 @@
-const PI =3.14;
-if(PI==="3.14"){
-    console.log("that is pi")
-}
-else{
-    console.log("that is not pi")
-}
+// const PI =3.14;
+// if(PI==="3.14"){
+//     console.log("that is pi")
+// }
+// else{
+//     console.log("that is not pi")
+// }
+
+
+//inequality operator
