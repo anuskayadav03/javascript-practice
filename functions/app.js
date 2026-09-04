8da8d400d54f49ec5a2b93423bb97555993bba23
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-04 13:12:16 +0530

    file updated

---
+++ functions/app.js
@@ -0,0 +1,9 @@
+function HappyBirthday(){
+    console.log("Happy birthday to you!");
+        console.log("Happy birthday to you!");
+    console.log("Happy birthday dear you!");
+    console.log("Happy birthday to you!");
+
+}
+
+HappyBirthday();
