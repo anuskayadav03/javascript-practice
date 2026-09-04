350bd3d2fdd3fe0291a1627fa1262b6b7bc3179d
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-04 13:14:20 +0530

    file updated

--- functions/app.js
+++ functions/app.js
@@ -1,9 +1,10 @@
-function HappyBirthday(){
+function HappyBirthday(username , age){
     console.log("Happy birthday to you!");
         console.log("Happy birthday to you!");
-    console.log("Happy birthday dear you!");
+    console.log(`Happy birthday dear ${username}!`);
     console.log("Happy birthday to you!");
+    console.log(`you are ${age} old !`);
 
 }
 
-HappyBirthday();
+HappyBirthday("anuska" ,20);
