76220025f20a2e0b59e54aef4ebc04a73f8d6e38
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-04 20:59:21 +0530

    file updated

--- Arrays/app.js
+++ Arrays/app.js
@@ -6,6 +6,11 @@ console.log(numoffrits);
 let index = fruits.indexOf("banana");
 console.log(index);
 
+fruits.sort();
+console.log(fruits);
+
+fruits.sort().reverse();
+console.log(fruits);
 
 // fruits[3]="coconut";
 fruits.push("cocnut");
