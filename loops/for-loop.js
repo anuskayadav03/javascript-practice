d4e38c3bc7f20584b13bb7fe1fed80a36c7539ca
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-04 11:38:53 +0530

    file updated

--- loops/for-loop.js
+++ loops/for-loop.js
@@ -2,7 +2,17 @@
 // console.log(i);
 // }
 
-for( let i=10;i>0;i--){
-console.log(i);
+// for( let i=10;i>0;i--){
+// console.log(i);
+// }
+// console.log("Happy new year!");
+
+
+for( let i=1;i<=20;i++){
+    if(i==13){
+        continue;
+    }
+    else{
+    console.log(i);
 }
-console.log("Happy new year!");
+}
