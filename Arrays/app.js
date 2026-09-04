f37025983dbbae410f0a5f8381db17fa54587318
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-04 20:54:52 +0530

    file updated

--- Arrays/app.js
+++ Arrays/app.js
@@ -1,6 +1,17 @@
 let fruits=["apple","orange","banana"];
 
+let numoffrits = fruits.length;
+console.log(numoffrits);
+
+let index = fruits.indexOf("banana");
+console.log(index);
+
+
 // fruits[3]="coconut";
+fruits.push("cocnut");
+fruits.pop();
+fruits.unshift("mango");
+
 
 console.log(fruits);
 console.log(fruits[2]);
