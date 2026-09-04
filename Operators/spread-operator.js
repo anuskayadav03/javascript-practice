c34b8e27a32320c5f9c7a26c4288afe7de4b504e
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-04 21:33:11 +0530

    file updated

---
+++ Operators/spread-operator.js
@@ -0,0 +1,16 @@
+// let num =[1,2,3,4,5,6,7];
+// let max = Math.max(...num);
+
+// console.log(max);
+
+// let username ="anuska yadav";
+// let letters =[...username].join("-");
+
+// console.log(letters);
+
+
+let fruits =["apple","banana","coconut"];
+let veggies=["carrots","celery","potato"];
+
+let food = [...fruits,...veggies, "eggs","milk"]
+console.log(food);
