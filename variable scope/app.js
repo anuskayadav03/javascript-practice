fcd1c2b3f20b1983f42dbbfe755368b4c7fd277c
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-04 13:50:45 +0530

    file updated

--- variable scope/app.js
+++ variable scope/app.js
@@ -10,3 +10,16 @@
 // }
 // function1();
 // function2();
+
+let x=1; 
+function function1(){
+    let x=10; 
+    console.log(x);
+}
+
+function function2(){
+    let x=15; 
+    console.log(x);
+}
+function1();
+function2();
