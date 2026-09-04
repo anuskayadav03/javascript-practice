ca13412e41a104a8b513f5752d47c32321c738fc
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-04 23:14:04 +0530

    file updated

---
+++ Operators/rest.js
@@ -0,0 +1,10 @@
+const food1="pizza";
+const food2 ="hamburger";
+const food3="hotdog";
+const food4="sushi";
+
+function openFridge(...food){
+    console.log(food);
+}
+
+openFridge(food1,food2,food3,food4);
