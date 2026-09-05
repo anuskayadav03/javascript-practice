1ad08da3cd86f3c74d51fac7194e7b97b567a9c7
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-05 17:10:14 +0530

    file updated

---
+++ arrow functions/app.js
@@ -0,0 +1,11 @@
+                        // SIMPLE WAY
+// function hello(){
+//     console.log("hello");
+// }
+// hello();
+
+
+                        //ARROW FUNCTION
+const hello = (name)=>console.log(`hello ${name}`);
+hello("anuska");            
+
