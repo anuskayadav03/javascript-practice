7ce445e9f0157d6d8bb3c5e403d897d3cfc2eea1
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-05 17:25:48 +0530

    file updated

--- objects/app.js
+++ objects/app.js
@@ -2,16 +2,27 @@ const person ={
     firstname:"anuska",
     lastname:"yadav",
     age:20,
-    isStudent:true
+    isStudent:true,
+    sayHello : function(){
+        console.log("hey i am anuska")},
 }
 const person2 ={
-    firstname:"anuska",
-    lastname:"yadav",
+    firstname:"kushal",
+    lastname:"shukla",
     age:20,
-    isStudent:true
+    isStudent:true,
+    sayHello : function(){
+        console.log("hey i am kushal")},
 }
 
 console.log(person.firstname);
 console.log(person.lastname);
 console.log(person.age);
 console.log(person.isStudent);
+person.sayHello();
+
+console.log(person2.firstname);
+console.log(person2.lastname);
+console.log(person2.age);
+console.log(person2.isStudent);
+person2.sayHello();
