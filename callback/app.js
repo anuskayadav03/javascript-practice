9f4e1a18403775409f5832bc60733657e4ae0a39
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-05 12:11:37 +0530

    file updated

--- callback/app.js
+++ callback/app.js
@@ -1,11 +1,21 @@
 
-hello(bye);
+// hello(bye);
 
-function hello(callback){
-    console.log("hello!");
-    callback();
+// function hello(callback){
+//     console.log("hello!");
+//     callback();
+// }
+
+// function bye(){
+//     console.log("good bye!");
+// }
+
+sum(display,1,2);
+
+function sum(callback ,x,y){
+    let result = x+y;
+    callback(result);
 }
-
-function bye(){
-    console.log("good bye!");
+function display(result){
+    console.log(result);
 }
