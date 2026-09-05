58669164a13c9aadbe0391429ced48093c999c5b
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-05 12:29:34 +0530

    file updated

--- loops/foreach.js
+++ loops/foreach.js
@@ -1,7 +1,11 @@
 let number =[1,2,3,4,5,6];
 
+number.forEach(double);
 number.forEach(display);
 
+function double(element,index,array){
+    array[index] =element*2;
+}
 function display(elements){
     console.log(elements);
 }
