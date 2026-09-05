d196ab258fe59c257611e84c3762e6cbc2eed3d8
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-05 13:39:24 +0530

    file updated

--- function expression/app.js
+++ function expression/app.js
@@ -7,3 +7,11 @@
 // setTimeout(() => {
 //     console.log("hello");
 // }, 3000);
+
+const numbers =[1,2,3,4,5,6,7];
+
+const squares =numbers.map(function square(elements){
+    return Math.pow(elements,2);
+});
+
+console.log(squares);
