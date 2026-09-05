608d0629a0b20d316dc95a87541f2d72a8f7d049
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-05 17:16:42 +0530

    file updated

---
+++ objects/app.js
@@ -0,0 +1,11 @@
+const person ={
+    firstname:"anuska",
+    lastname:"yadav",
+    age:20,
+    isStudent:true
+}
+
+console.log(person.firstname);
+console.log(person.lastname);
+console.log(person.age);
+console.log(person.isStudent);
