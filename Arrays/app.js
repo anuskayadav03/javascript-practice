ec85d6b9a91af1f7cfcaba01c5881b1b5f8fa74b
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-06 20:17:03 +0530

    file added

--- Arrays/app.js
+++ Arrays/app.js
@@ -1,16 +1,16 @@
 let fruits=["apple","orange","banana"];
 
-let numoffrits = fruits.length;
-console.log(numoffrits);
+// let numoffrits = fruits.length;
+// console.log(numoffrits);
 
-let index = fruits.indexOf("banana");
-console.log(index);
+// let index = fruits.indexOf("banana");
+// console.log(index);
 
-fruits.sort();
-console.log(fruits);
+// fruits.sort();
+// console.log(fruits);
 
-fruits.sort().reverse();
-console.log(fruits);
+// fruits.sort().reverse();
+// console.log(fruits);
 
 // fruits[3]="coconut";
 fruits.push("cocnut");
