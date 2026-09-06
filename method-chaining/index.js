05a8262c89ec41db514e89d334a32018bdd17f0a
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-06 20:16:32 +0530

    file added

--- method-chaining/index.js
+++ method-chaining/index.js
@@ -18,6 +18,6 @@
 
 // ---METHOD CHAINING --
 
-let username = window.prompt("enter your username : ");
- username = username.trim().charAt(0).toUpperCase() + username.slice(1).toLowerCase();
- console.log(username);
+// let username = window.prompt("enter your username : ");
+//  username = username.trim().charAt(0).toUpperCase() + username.slice(1).toLowerCase();
+//  console.log(username);
