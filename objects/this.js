d9ddb210ebd6dfeeb81b1379506bc5f2840fc0ba
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-07 10:09:47 +0530

    file added

--- objects/this.js
+++ objects/this.js
@@ -2,7 +2,7 @@ const person1 ={
     name: "anuska",
     favFood:"pizza",
     sayHello:function(){
-        console.log("hey i am anuska")
+        console.log(`hey i am ${this.name}`)
     }
 }
 
