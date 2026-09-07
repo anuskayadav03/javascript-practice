5ad839f0c9f5974e948679d1133082bde2915434
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-07 10:19:24 +0530

    file added

--- constructors/app.js
+++ constructors/app.js
@@ -5,9 +5,16 @@ this.year=year,
 this.color=color
 }
 const car1 =new Car("ford","mustang",2024,"red");
+const car2 =new Car("chevrolet","camaro",2025,"blue");
 
 console.log(car1);
 console.log(car1.make);
 console.log(car1.model);
 console.log(car1.year);
 console.log(car1.color);
+
+console.log(car2);
+console.log(car2.make);
+console.log(car2.model);
+console.log(car2.year);
+console.log(car2.color);
