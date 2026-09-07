28bd91f4b8835967ccd1cf7b90e5a8c411a404ed
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-07 11:08:47 +0530

    file updated

--- basics/static.js
+++ basics/static.js
@@ -1,4 +1,30 @@
-class Mathutil{
-    static PI = 3.1415;
+// class Mathutil{
+//     static PI = 3.1415;
+//     static getDiameter(radius){
+//         return radius*2;
+//     }
+// }
+// console.log(Mathutil.PI);
+// console.log(Mathutil.getDiameter(10));
+
+
+class user{
+    static userCount=0;
+
+    constructor(username){
+         this.username=username;
+         username++;
+    }
+    sayHello(){
+        console.log(`hello my name is ${this.username}`)
+    }
 }
-console.log(Mathutil.PI);
+
+const user1 = new user("anuska");
+const user2 = new user("kushal");
+const user3 = new user("vedika");
+
+user1.sayHello();
+user2.sayHello();
+user3.sayHello();
+console.log(user.userCount);
