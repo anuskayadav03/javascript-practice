dd00db4a7a89f544b8ec7539f6fc2aceea835197
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-07 10:41:13 +0530

    file added

--- class/app.js
+++ class/app.js
@@ -3,8 +3,13 @@ class products {
         this.name=name,
         this.price=price
     }
-}
 
 displayproduct(){
-    
+    console.log(`prouct name: ${this.name}`);
+     console.log(`prouct name: ${this.price}`);
 }
+}
+
+const product1 = new products("shirt",20);
+
+product1.displayproduct();
