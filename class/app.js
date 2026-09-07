4e3b5c60ba3fd55fb531106b5a5e4d80fccfd581
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-07 10:52:18 +0530

    file updated

--- class/app.js
+++ class/app.js
@@ -8,10 +8,20 @@ displayproduct(){
     console.log(`prouct name: ${this.name}`);
      console.log(`prouct name: ${this.price}`);
 }
+  calculateTotal(saleTax){
+    return this.price +(this.price*saleTax);
+  }
 }
+const saleTax =0.05;
 
 const product1 = new products("shirt",200);
 const product2 = new products("pants",300);
 
 product1.displayproduct();
 product2.displayproduct();
+
+const total = product1.calculateTotal(saleTax);
+console.log(`your total amount is ${total}`);
+
+const total2 = product2.calculateTotal(saleTax);
+console.log(`your total amount is ${total2}`);
