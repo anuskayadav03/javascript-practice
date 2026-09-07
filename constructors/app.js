cca8746117e8c4fe54c89150308dc071ea5bf9ea
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-07 10:15:55 +0530

    file added

---
+++ constructors/app.js
@@ -0,0 +1,9 @@
+function Car(make,model,year,color){
+this.make=make,
+this.model=model,
+this.year=year,
+this.color=color
+}
+const car1 =new car("ford","mustang",2024,"red");
+
+console.log(car1.make);
