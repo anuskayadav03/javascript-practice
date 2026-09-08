d6ccd55d71c995bad847f2c74ac69b45f60fca33
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-08 22:12:00 +0530

    file updated

---
+++ objects/nested.js
@@ -0,0 +1,20 @@
+const person ={
+    fullName:"anuska",
+    age:20,
+    isStudent:true,
+    hobby: ["cooking","reading"],
+    address :{
+        street:"123 xyz",
+        city:"dehradun",
+        country:"india"
+    }
+}
+console.log(person.fullName);
+console.log(person.age);
+console.log(person.isStudent);
+console.log(person.hobby[1]);
+console.log(person.hobby[0]);
+console.log(person.address);
+console.log(person.address.street);
+console.log(person.address.city);
+console.log(person.address.country);
