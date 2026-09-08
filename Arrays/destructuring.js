d66038ea0478d63f571bdca809d6eb228796a397
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-08 21:24:33 +0530

    file updated

--- Arrays/destructuring.js
+++ Arrays/destructuring.js
@@ -8,7 +8,19 @@
 // console.log(b);
 
 //SWAP 2 ELEMENTS IN AN ARRAY
-const colors=["red","green","pink","blue"];
-[colors[0], colors[3]] =[colors[3],colors[0]];
+// const colors=["red","green","pink","blue"];
+// [colors[0], colors[3]] =[colors[3],colors[0]];
+
+// console.log(colors);
+
+
+//ASSIGN ARRAY ELEMENTS TO VARIABLES
+const colors=["red","green","blue","white"];
+
+const [firstColor,secondColor,thirdColor,fourthColor] =colors;
 
 console.log(colors);
+console.log(firstColor);
+console.log(secondColor);
+console.log(thirdColor);
+console.log(fourthColor);
