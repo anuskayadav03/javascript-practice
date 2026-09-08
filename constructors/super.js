ba6d3bbe40eb4203cf8888b3c9d10aeb19e23601
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-08 12:24:06 +0530

    file updated

--- constructors/super.js
+++ constructors/super.js
@@ -10,8 +10,20 @@ constructor(name,age,runspeed){
     this.runspeed=runspeed;
 }
 }
+
+class Fish extends Animal{
+constructor(name,age,swimspeed){
+    super(name,age);
+    this.swimspeed=swimspeed;
+}
+}
 const rabbit = new Rabbit("rabbit",1,25);
+const fish = new Fish("fish",5,225);
 
 console.log(rabbit.name);
 console.log(rabbit.age);
 console.log(rabbit.runspeed);
+
+console.log(fish.name);
+console.log(fish.age);
+console.log(fish.swimspeed);
