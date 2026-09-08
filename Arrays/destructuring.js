23a066e259cef3a9386c3db41fc8a463f3762e47
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-08 20:48:14 +0530

    file updated

--- Arrays/destructuring.js
+++ Arrays/destructuring.js
@@ -1,8 +1,14 @@
 //SWAP THE VALUE OF TWO VARIABLES 
 
-let a=1;
-let b=2;
+// let a=1;
+// let b=2;
 
-[a,b]=[b,a];
-console.log(a);
-console.log(b);
+// [a,b]=[b,a];
+// console.log(a);
+// console.log(b);
+
+//SWAP 2 ELEMENTS IN AN ARRAY
+const colors=["red","green","pink","blue"];
+[colors[0], colors[3]] =[colors[3],colors[0]];
+
+console.log(colors);
