314efe13743c5ecdae0a44f3a326d81134b8d209
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-09 12:52:26 +0530

    file updated

--- data object/app.js
+++ data object/app.js
@@ -1,3 +1,19 @@
-const date = new Date(2024,0,1,2,3,5);
+// const date = new Date(2024,0,1,2,3,5);
+// const date = new Date(1.700000000000000000);
+const date = new Date();
+
+const year =date.getFullYear();
+ const day = date.getDate();
+const hours = date.getHours();
+const min = date.getMinutes();
+const sec = date.getSeconds();
+const dayofweek = date.getDay();
+
 
 console.log(date);
+console.log(year);
+console.log(day);
+console.log(hours);
+console.log(min);
+console.log(sec);
+console.log(dayofweek);
