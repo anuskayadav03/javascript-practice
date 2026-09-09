511d390e44976cee8a99e8600c2b91f27439c39f
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-09 06:11:52 +0530

    file updated

--- Arrays/array of objects/app.js
+++ Arrays/array of objects/app.js
@@ -3,7 +3,9 @@ const fruits = [
      {name:"orange", color:"orange",calories:135},
       {name:"mango", color:"yello",calories:225},
 ]
-fruits.splice(0,2);
+// fruits.splice(0,2);
 
-fruits.push({name:"grapes" , color:"green"});
-console.log(fruits);
+// fruits.push({name:"grapes" , color:"green"});
+// console.log(fruits);
+
+fruits.forEach(fruit => console.log(fruit.name));
