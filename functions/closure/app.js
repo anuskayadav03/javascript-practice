808672a07294e289c29c68b5486c020354b74ebd
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-09 21:21:59 +0530

    file updated

--- functions/closure/app.js
+++ functions/closure/app.js
@@ -1,10 +1,28 @@
-function outer(){
-    let message ="hello";
-    function inner(){
-console.log(message);
-    }
-    inner();
+// function outer(){
+
+//     let message ="hello";
+
+//     function inner(){
+// console.log(message);
+//     }
+//     inner();
+// }
+
+
+// outer();
+function createCounter(){
+    let count=0;
+
+function increment(){
+    count++;
+    console.log(`count increased to ${count}`);
+
+}
+return{increment};
 }
 
+const counter = createCounter();
 
-outer();
+counter.increment();
+counter.increment();
+counter.increment();
