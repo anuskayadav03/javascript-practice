fd5eb03600d5542e1c5392db5d727f472339fd5b
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-09 07:29:10 +0530

    file updated

--- Arrays/sorting/app.js
+++ Arrays/sorting/app.js
@@ -1,3 +1,7 @@
-let fruits = ["apple","orange","banana","pineapple"];
-fruits.sort();
-console.log(fruits);
+// let fruits = ["apple","orange","banana","pineapple"];
+// fruits.sort();
+// console.log(fruits);
+
+let number =[1,7,9,40,30,22,11,12,13];
+number.sort((a,b)=> a-b );
+console.log(number);
