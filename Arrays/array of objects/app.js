93eb9e973ef4404d14e8523d861e6231efdd5dbe
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-09 06:07:01 +0530

    file updated

--- Arrays/array of objects/app.js
+++ Arrays/array of objects/app.js
@@ -1,9 +1,9 @@
 const fruits = [
     {name:"apple", color:"red",calories:95},
      {name:"orange", color:"orange",calories:135},
-      {name:"mangp", color:"yello",calories:225},
+      {name:"mango", color:"yello",calories:225},
 ]
-fruits.splice(0,1);
-console.log(fruits[0].name);
+fruits.splice(0,2);
 
 fruits.push({name:"grapes" , color:"green"});
+console.log(fruits);
