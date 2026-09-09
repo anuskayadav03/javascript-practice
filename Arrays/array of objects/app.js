565cfd7c0726d1a417f1abbb1b9ed294c2325731
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-09-09 06:33:54 +0530

    file updated

--- Arrays/array of objects/app.js
+++ Arrays/array of objects/app.js
@@ -1,7 +1,7 @@
 const fruits = [
     {name:"apple", color:"red",calories:95},
      {name:"orange", color:"orange",calories:135},
-      {name:"mango", color:"yello",calories:225},
+      {name:"mango", color:"yellow",calories:225},
 ]
 // fruits.splice(0,2);
 
@@ -10,5 +10,8 @@ const fruits = [
 
 // fruits.forEach(fruit => console.log(fruit.name));
 
-const fruitsName = fruits.map(fruit => fruit.name);
-console.log(fruitsName);
+// const fruitsName = fruits.map(fruit => fruit.name);
+// console.log(fruitsName);
+
+const yellofruits = fruits.filter(fruit => fruit.color === "yellow");
+console.log(yellofruits);
